cd8406ae4d46963fc17f02e3a65dad8d37e5c12d
Melia <mjihadah@ad.unc.edu>
2022-01-21 18:43:48 -0500

    finished ex01

--- exercises/ex01_chardle.py
+++ exercises/ex01_chardle.py
@@ -15,23 +15,25 @@ matches: int = 0
 print("Searching for " + single_char + " in " + five_letter_word)
 
 if five_letter_word[0] == single_char: 
-    print(single_char + " was found at index 0.")
+    print(single_char + " found at index 0.")
     matches = matches + 1
 if five_letter_word[1] == single_char:
-    print(single_char + " was found at index 1.")
+    print(single_char + " found at index 1.")
     matches = matches + 1
 if five_letter_word[2] == single_char:
-    print(single_char + " was found at index 2.")
+    print(single_char + " found at index 2.")
     matches = matches + 1
 if five_letter_word[3] == single_char:
-    print(single_char + " was found at index 3.")
+    print(single_char + " found at index 3.")
     matches = matches + 1
 if five_letter_word[4] == single_char:
-    print(single_char + " was found at index 4.")
+    print(single_char + " found at index 4.")
     matches = matches + 1
 
 if matches == 0:
     print("No instances of " + single_char + " found in " + five_letter_word)
+if matches == 1:
+    print("1 instance of " + single_char + " found in " + five_letter_word)
 else:
     print(str(matches) + " instances of " + single_char + " found in " + five_letter_word)
 # variable 'matches' needs to count the number of matching characters, somehow, and use it to tell people how many instances of each character there are

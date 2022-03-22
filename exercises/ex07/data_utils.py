03a74581fe840f6896b256721682cf33ba20d4f5
Melia <mjihadah@ad.unc.edu>
2022-03-22 12:35:46 -0400

    completed EX07

--- exercises/ex07/data_utils.py
+++ exercises/ex07/data_utils.py
@@ -49,4 +49,56 @@ def head(table: dict[str, list[str]], N: int) -> dict[str, list[str]]:
     """Produce a column-based table with only the first N rows of data."""
     result: dict[str, list[str]] = {}
 
+    for column in table:
+        storage: list[str] = []
+
+        if N >= len(table):
+            result = table
+        else:
+            i = 0
+            while i < N:
+                storage.append(table[column][i])
+                i += 1
+            result[column] = storage
+
+    return result
+
+
+def select(table: dict[str, list[str]], columns: list[str]) -> dict[str, list[str]]:
+    """Produce a new column-based table with only a specific subset of the original columns."""
+    result: dict[str, list[str]] = {}
+    
+    for column in columns:
+        result[column] = table[column]
+    
+    return result
+
+
+def concat(table_one: dict[str, list[str]], table_two: dict[str, list[str]]) -> dict[str, list[str]]:
+    """Produce a new column-based table with two column-based tables combined."""
+    result: dict[str, list[str]] = {}
+
+    for column in table_one:
+        result[column] = table_one[column]
+
+    for column in table_two:
+        if column in result:
+            for items in table_two[column]:
+                result[column].append(items)
+        else:
+            result[column] = table_two[column]
+    
+    return result
+
+
+def count(input: list[str]) -> dict[str, int]:
+    """Given a list, this function will produce a dictionry that tracks the frequency of each unique item value."""
+    result: dict[str, int] = {}
+
+    for item in input:
+        if item in result:
+            result[item] = result[item] + 1
+        else:
+            result[item] = 1
+    
     return result

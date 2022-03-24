1caab2492f2a1bc5a8f3c2b30eb9c62cc39877eb
Melia <mjihadah@ad.unc.edu>
2022-03-24 10:45:41 -0400

    3.24 class 19

---
+++ lessons/union-type.py
@@ -0,0 +1,15 @@
+"""Union type give flexibility to single vars."""
+
+from typing import Union
+
+
+def log(info: Union[str, int]) -> None:
+    """Info can be str or int."""
+    if isinstance(info, str):
+        print(f'str: {info}')
+    else:
+        print(f'int: {info}')
+
+
+log("hello")
+log(110)
